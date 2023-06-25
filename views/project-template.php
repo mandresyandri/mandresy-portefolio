b78d89a37e915d7a285e1e2cd16df4af3e2efbb9
mandresyandri <manjess.andri@icloud.com>
2023-06-25 18:17:22 +0200

    Ok pour la partie blog

--- views/project-template.php
+++ views/project-template.php
@@ -2,40 +2,50 @@
 <?php
 // Collecte des données dans l'article
 include("../tools/api_notion.php");
-/*$pageID = $_GET["id"];*/
-/*$getPageData = array(pageRequests($pageID));*/
+$pageID = $_GET["id"];
+$getPageData = array(pageRequests($pageID));
 ?>
-    <!-- MAIN PAGE CONTENT -->
+    <!-- DISPOSITION DU TITRE -->
     <div id="main-content">
-      <!-- PROJECT HEADER -->
-      <div id="project-header">
-        <div class="main-title">Mon titre</div>
-        <image class="project-header-image" src="<?php echo "Image support"/*$getPageData[0]['results'][0]['image']['file']['url']*/;?>">
-        <hr/>
-      </div>
-
-      <!-- PROJECT DETAILS -->
-      <div id="project-details">
-        <div class="subheader-text">Titre</div>
-        <div class="project-details-content">
-          <div class="body-text"><?php echo "Long texte à insérer ici !"/*echo $getPageData[0]['results'][3]["paragraph"]["rich_text"][0]["text"]["content"]*/ ?></div>
-          <div class="body-text">Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida. Aliquet nec ullamcorper sit amet. Aliquet sagittis id consectetur purus. Ac tortor dignissim convallis aenean. Enim neque volutpat ac tincidunt vitae. Lobortis feugiat vivamus at augue. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Viverra orci sagittis eu volutpat. Massa tincidunt nunc pulvinar sapien et ligula. Donec enim diam vulputate ut.</div>
+        <div id="project-header">
+            <div class="main-title"><?php echo $getPageData[0]['results'][0]['heading_1']['rich_text'][0]['text']['content']; ?></div>
+            <image class="project-header-image" src="<?php echo $getPageData[0]['results'][1]['image']['file']['url'];?>">
         </div>
-      </div>
+
+        <!-- PROJECT DETAILS -->
+        <div id="project-details">
+            <div class="subheader-text"><?php echo $getPageData[0]['results'][2]['heading_2']['rich_text'][0]['text']['content'];?></div>
+            <div class="project-details-content">
+                <div class="body-text"><?php echo $getPageData[0]['results'][3]['paragraph']['rich_text'][0]['text']['content'];?></div>
+            </div>
+        </div>
+
+        <div id="project-details">
+            <div class="subheader-text"><?php echo $getPageData[0]['results'][4]['heading_2']['rich_text'][0]['text']['content'];?></div>
+            <div class="project-details-content">
+                <div class="body-text"><?php echo $getPageData[0]['results'][5]['paragraph']['rich_text'][0]['text']['content'];?></div>
+            </div>
+        </div>
+
+        <div id="project-details">
+            <div class="subheader-text"><?php echo $getPageData[0]['results'][6]['heading_2']['rich_text'][0]['text']['content'];?></div>
+            <div class="project-details-content">
+                <div class="body-text"><?php echo $getPageData[0]['results'][7]['paragraph']['rich_text'][0]['text']['content'];?></div>
+            </div>
+        </div>
 
       <!-- Aperçu du projet -->
       <div id="project-gallery">
         <div class="subheader-text">Aperçu / Code source</div>
         <div class="project-gallery-content">
-            <div class="gallery-image-container half-width">
-              <img src="../assets/images/code.jpeg" class="gallery-image">
-              <span class="image-caption">Accéder au notebook</span>
-            </div>
-            <!-- S'il n'y a pas de lien vers le projet => Ne pas mettre cette partie -->
-            <div class="gallery-image-container half-width">
-              <img src="../assets/images/sketchbook.jpeg" class="gallery-image">
-              <span class="image-caption">Code source sur GitHub</span>
-            </div>
+            <a href="<?php echo $getPageData[0]['results'][10]['paragraph']['rich_text'][0]["text"]["content"];?>" target="_blank">
+                <div class="gallery-image-container half-width">
+                  <img src="<?php echo $getPageData[0]['results'][9]['image']['file']['url'];?>" class="gallery-image">
+                    <span class="image-caption">
+                            Accéder au notebook
+                    </span>
+                </div>
+            </a>
         </div>
       </div>
     </div>
