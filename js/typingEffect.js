be4d57436ea5414e759b8c0b82679395741d06d8
mandresyandri <manjess.andri@icloud.com>
2023-06-25 19:37:07 +0200

    Ajout text code js

--- js/typingEffect.js
+++ js/typingEffect.js
@@ -4,7 +4,7 @@ new Typewriter(typingText, {
   strings: [
     `<a class="hashtag">#dataviz</a> <a class="hashtag">#machinelearning</a> <a class="hashtag">#deeplearning</a>.`,
     `<a class="hashtag">#pandas</a> <a class="hashtag">#d3.js</a> <a class="hashtag">#powerbi</a> <a class="hashtag">#sql</a>.`,
-    `<a class="hashtag">#sklearn</a>.`
+    `<a class="hashtag">#sklearn</a> <a class="hashtag">#tensorflow</a>..`
 ],
   autoStart: true,
   loop: true,
